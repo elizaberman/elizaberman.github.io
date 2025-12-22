6d41847a49e1fc9871b6fcce7b4246682a98d426
elizaberman <61032851+elizaberman@users.noreply.github.com>
2025-12-22 02:38:05 +0000

    Deploying to gh-pages from @ elizaberman/elizaberman.github.io@fe637526a0e0a63ab35e4a9ca07a20e31f700b15 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -441,13 +441,6 @@ ninja.data = [{
           window.open("/assets/pdf/example_pdf.pdf", "_blank");
         },
       },{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
         id: 'social-linkedin',
         title: 'LinkedIn',
         section: 'Socials',
