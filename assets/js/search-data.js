d7860d860552e4f1e0c929e7fff018abb1d033d6
elizaberman <61032851+elizaberman@users.noreply.github.com>
2025-12-22 01:23:06 +0000

    Deploying to gh-pages from @ elizaberman/elizaberman.github.io@803947203124d793e97a850feec5403e853277a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -455,13 +455,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/eliza-berman-2a39861b7", "_blank");
         },
       },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0009000955494822", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
